d10edd023b1d09464a28b0d845e7eb2d448b82f7
https://github.com/jamshidjanibekov <jamshidjanibekov0@gmail.com>
2025-11-15 21:56:35 +0500

    eslint edit

--- app/[lng]/(root)/(home)/page.tsx
+++ app/[lng]/(root)/(home)/page.tsx
@@ -4,10 +4,10 @@ import { useTranslation } from '@/i18n/client'
 import { useParams } from 'next/navigation'
 
 function Page() {
-  const { lng } = useParams()
-  const { t } = useTranslation(lng as string)
+	const { lng } = useParams()
+	const { t } = useTranslation(lng as string)
 
-  return <div className='mt-24'>{t('home')}</div>
+	return <div className='mt-24'>{t('home')}</div>
 }
 
 export default Page
--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -1,11 +1,13 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 const Logo = () => {
-  return (
-    <Link href="/" className='flex  items-center gap-4'>
-        <h1 className='cursor-pointer font-space-grotesk text-4xl font-bold'>Jamshid</h1>
-    </Link>
-  );
-};
+	return (
+		<Link href='/' className='flex  items-center gap-4'>
+			<h1 className='cursor-pointer font-space-grotesk text-4xl font-bold'>
+				Udemy
+			</h1>
+		</Link>
+	)
+}
 
-export default Logo;
+export default Logo
