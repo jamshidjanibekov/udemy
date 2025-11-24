1a3d64a44f0b275dac20649956f2ffd703f91a3c
https://github.com/jamshidjanibekov <jamshidjanibekov0@gmail.com>
2025-11-24 11:19:27 +0500

    home page ui

--- app/[lng]/(root)/layout.tsx
+++ app/[lng]/(root)/layout.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import {ChildProps} from "@/types";
-import Navbar from "@/app/[lng]/(root)/_components/navbar";
-const Layout = ({children}:ChildProps) => {
-  return (
-    <div>
-      <Navbar/>
-      <main>
-        {children}
-      </main>
-    </div>
-  );
-};
+import React from 'react'
+import { ChildProps } from '@/types'
+import Navbar from '@/app/[lng]/(root)/_components/navbar'
+import Footer from './_components/footer'
+const Layout = ({ children }: ChildProps) => {
+	return (
+		<div>
+			<Navbar />
+			<main>{children}</main>
+			<Footer />
+		</div>
+	)
+}
 
-export default Layout;
+export default Layout
--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,31 @@
-import {ReactNode} from "react";
+import { ReactNode } from 'react'
 
-export interface ChildProps{
-  children: ReactNode;
+export interface ChildProps {
+	children: ReactNode
 }
-export interface LngParams{
-  params:{lng:string}
+export interface LngParams {
+	params: { lng: string }
+}
+
+export interface ICourse {
+	title: string
+	previewImage: string
+	level: string
+	author: {
+		image: string
+		name: string
+	}
+	oldPrice: number
+	currentPrice: number
+}
+
+export interface iCategory {
+	label: string
+	icon: string
+}
+
+export interface IInstructor {
+	name: string
+	image: string
+	job: string
 }
