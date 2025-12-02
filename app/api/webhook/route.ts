65f551a637bb62447abe5adb72c6693298f9e64d
https://github.com/jamshidjanibekov <jamshidjanibekov0@gmail.com>
2025-12-02 17:50:59 +0500

    fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -50,13 +50,14 @@ export async function POST(req: Request) {
 	const eventType = evt.type
 
 	if (eventType === 'user.created') {
-		const { id, email_addresses, image_url, first_name, last_name } = evt.data
+		const { id, email_addresses, profile_image_url, first_name, last_name } =
+			evt.data
 
 		const user = await createUser({
 			clerkId: id,
 			email: email_addresses[0].email_address,
 			fullName: `${first_name} ${last_name}`,
-			picture: image_url,
+			picture: profile_image_url,
 		})
 
 		return NextResponse.json({ message: 'OK', user })
