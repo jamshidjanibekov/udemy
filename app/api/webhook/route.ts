85112f04a642432e2e082463d5594fd797bc365b
https://github.com/jamshidjanibekov <jamshidjanibekov0@gmail.com>
2025-12-02 17:33:00 +0500

    webhook edit

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,3 +1,7 @@
+export const dynamic = 'force-dynamic'
+export const runtime = 'nodejs'
+export const revalidate = 0
+
 /* eslint-disable camelcase */
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
